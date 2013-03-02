24b51bf2150ff58585f75a6636bdbc722ff3eb59
Rayjan Wilson <rayjan.wilson@gmail.com>
2013-03-02 10:56:46 -0900

    ok got some basic file upload happening correctly.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , format = require('util').format
   , path = require('path');
 
 var app = express();
@@ -38,10 +39,14 @@ app.get('/home', routes.home);
 app.get('/about', routes.about);
 app.get('/upload', routes.upload);
 
-app.post('/upload', function(req, res) {
-    console.log(req.body);
-    console.log(req.files);
-    res.send(200);
+app.post('/upload', function(req, res, next){
+  // the uploaded file can be found as `req.files.image` and the
+  // title field as `req.body.title`
+  res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+    , req.files.flightlog.name
+    , req.files.flightlog.size / 1024 | 0 
+    , req.files.flightlog.path
+    , req.body.title));
 });
 
 http.createServer(app).listen(app.get('port'), function(){
